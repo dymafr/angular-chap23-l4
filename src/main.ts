01caf1b9bdb81bee77187ec82bb3b6062f43e456
dymafr <32801166+dymafr@users.noreply.github.com>
2023-09-07 13:20:48 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import 'zone.js';
+import 'zone.js'; // pour Stackblitz uniquement
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
